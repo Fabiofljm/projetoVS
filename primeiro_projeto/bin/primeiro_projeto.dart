a8e10b8b3b5597dd57212820af3ccce59619a159
fabio <fabio_fljm@hotmail.com>
2022-09-16 10:53:56 -0400

    primeira_alteração

--- primeiro_projeto/bin/primeiro_projeto.dart
+++ primeiro_projeto/bin/primeiro_projeto.dart
@@ -2,5 +2,11 @@ import 'package:primeiro_projeto/primeiro_projeto.dart' as primeiro_projeto;
 
 void main(List<String> arguments) {
   int x = 10;
+
+  int y = 100;
+
+  x = y - x;
+  
+  y=700;
   print('Hello world: ${primeiro_projeto.calculate()}!');
 }
