058f26eb3c0f4faacf0e307340665cd3245dcff5
fabio <fabio_fljm@hotmail.com>
2022-09-16 10:18:49 -0400

    add var

---
+++ primeiro_projeto/bin/primeiro_projeto.dart
@@ -0,0 +1,6 @@
+import 'package:primeiro_projeto/primeiro_projeto.dart' as primeiro_projeto;
+
+void main(List<String> arguments) {
+  int x = 10;
+  print('Hello world: ${primeiro_projeto.calculate()}!');
+}
